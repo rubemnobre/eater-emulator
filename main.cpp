02398f65045d61d96612067327b6ba9faa1fdd1b
rubemnobre <rubemjrx@hotmail.com>
2020-06-23 12:01:31 -0300

    add timeout option

--- main.cpp
+++ main.cpp
@@ -6,8 +6,10 @@
 #include <getopt.h>
 #include <cstdint>
 #include <cstdio>
+#include <string>
+#include <iostream>
 
-static constexpr char helptext[] = "usage: \n-f <file> : loads the binary file into RAM\n-r <file> : loads the binary file for the instructions rom\n-d : run with debug option on\n-s : signed output\n";
+static constexpr char helptext[] = "usage: \n-f <file> : loads the binary file into RAM\n-r <file> : loads the binary file for the instructions rom\n-d : run with debug option on\n-s : signed output\n-t <n>: timeout after <n> instructions";
 static constexpr int STEPS_PER_INSTRUCTION = 5;
 
 
@@ -15,6 +17,7 @@ int main(int argc, char** argv){
     bool SIGNED_OUT = false;
     bool DEBUG = false;
     bool newInstructions = false;
+    int timeout = -1;
     std::array<std::uint8_t, RAM::RAM_SIZE> ram = RAM::loadNull();
     std::array<std::array<std::uint16_t, STEPS_PER_INSTRUCTION>, 16> instructions = logic<STEPS_PER_INSTRUCTION>::defaultInstructions;
     
@@ -26,9 +29,20 @@ int main(int argc, char** argv){
     }
 
     char c;
-    while((c = getopt(argc, argv, "hf:r:sd")) != -1){
+    while((c = getopt(argc, argv, "hf:r:sdt:")) != -1){
         switch (c)
         {
+            case 't':
+                try{
+                    timeout = std::stoi(optarg);   
+                }catch(std::invalid_argument const &e){
+                    printf("Invalid timeout argument\n");
+                    return 1;
+                }catch(std::out_of_range const &e){
+                    printf("Timeout too large\n");
+                    return 1;
+                }
+                break;
             case 'h':
                 printf("%s", helptext);
                 return 0;
@@ -63,7 +77,10 @@ int main(int argc, char** argv){
 
     logic <STEPS_PER_INSTRUCTION> CPU(ram, instructions, DEBUG);
 
+    int cycles = 0;
+
     while(!CPU.halt) {
+        cycles++;
         CPU.cycle();
         if(CPU.outputNow){   //Print the output register if its value changes
             if(SIGNED_OUT)
@@ -71,6 +88,7 @@ int main(int argc, char** argv){
             else
                 printf("%d\n", static_cast<std::uint8_t>(CPU.outReg));
         }
+        if(cycles/STEPS_PER_INSTRUCTION >= timeout && timeout != -1) break;
     }
     return 0;
 }
