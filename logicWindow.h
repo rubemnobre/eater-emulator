cd497e1ffb7913ad1363afd367cb3135cf0dffe9
rubemnobre <rubemjrx@hotmail.com>
2020-06-25 13:44:42 -0300

    add output to gui

--- logicWindow.h
+++ logicWindow.h
@@ -18,25 +18,33 @@ class logicWindow : public Gtk::Window{
         {
             set_border_width(10);
             set_title("eater-emulator");
-            
-            nextButton.signal_clicked().connect(sigc::mem_fun(CPU, &logic<STEPS_PER_INSTRUCTION>::cycle));
+        
+
+            nextButton.signal_clicked().connect(sigc::mem_fun(*this, &logicWindow::cpuCycle));
             nextButton.set_border_width(10);
 
             runFree.signal_clicked().connect(sigc::mem_fun(*this, &logicWindow::set_runFree));
             
             clockSpeed.get_adjustment()->signal_value_changed().connect(sigc::mem_fun(*this, &logicWindow::set_runFree));
             
+            output.set_text("0");
+
             add(grid);
-            grid.attach(runFree   , 1, 0, 1, 1);
-            grid.attach(nextButton, 0, 0, 1, 1);
-            grid.attach(clockSpeed, 0, 1, 2, 1);
+            grid.attach(output    , 0, 0, 2, 1);
+            grid.attach(runFree   , 1, 1, 1, 1);
+            grid.attach(nextButton, 0, 1, 1, 1);
+            grid.attach(clockSpeed, 0, 3, 2, 1);
 
             show_all_children();
         }
-        bool cpuCycle(int arg1){
+        void cpuCycle(){
             if(!CPU.halt){
                 CPU.cycle();
+                output.set_text(std::to_string(CPU.outReg));
             }
+        }
+        bool cpu_cycle_callback(int arg){
+            cpuCycle();
             return true;
         }
         void set_runFree(){
@@ -45,7 +53,7 @@ class logicWindow : public Gtk::Window{
                     timeoutConn.disconnect();
                 }
                 unsigned int interval = 1000/clockSpeed.get_value(); 
-                sigc::slot<bool> cycleSlot = sigc::bind(sigc::mem_fun(*this, &logicWindow::cpuCycle), 0);
+                sigc::slot<bool> cycleSlot = sigc::bind(sigc::mem_fun(*this, &logicWindow::cpu_cycle_callback), 0);
                 timeoutConn = Glib::signal_timeout().connect(cycleSlot, interval);
             }else{
                 timeoutConn.disconnect();
