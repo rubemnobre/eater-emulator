db1bee2f4c0a9e6aff98a3339fc1816e3255fa00
rubemnobre <rubemjrx@hotmail.com>
2020-06-26 01:52:37 -0300

    add ram management on gui

--- logicWindow.h
+++ logicWindow.h
@@ -4,25 +4,34 @@
 #include <gtkmm-3.0/gtkmm.h>
 #include "logic.h"
 #include "ram.h"
+#include <iomanip>
 
 constexpr int STEPS_PER_INSTRUCTION = 5;
 
 
 class logicWindow : public Gtk::Window{
     public:
-        logic<STEPS_PER_INSTRUCTION> CPU;
         logicWindow() :
             nextButton("Clock Cycle"),
             nextInstructionButton("Next Instruction"),
-            clockSpeed(5, 510, 10)
+            ramLoad("Load"),
+            clockSpeed(5, 510, 10),
+            ramAddress(Gtk::Adjustment::create(0, 0, 16, 1, 1, 1))
         {
             set_border_width(10);
             set_title("eater-emulator");
+
+            sigc::slot<bool> slot = sigc::bind(sigc::mem_fun(*this, &logicWindow::update_callback), 1);
+            updateConn = Glib::signal_timeout().connect(slot, 100);
 
             runFree.signal_state_flags_changed().connect(sigc::mem_fun(*this, &logicWindow::set_runFree));
 
             nextInstructionButton.signal_clicked().connect(sigc::mem_fun(*this, &logicWindow::cpu_inst_callback));
             nextInstructionButton.set_border_width(10);
+
+            ramValue.signal_activate().connect(sigc::mem_fun(*this, &logicWindow::change_ram));
+            ramAddress.signal_activate().connect(sigc::mem_fun(ramValue, &Gtk::Widget::grab_focus));
+            ramLoad.signal_clicked().connect(sigc::mem_fun(*this, &logicWindow::load_ram));
 
             nextButton.signal_clicked().connect(sigc::mem_fun(*this, &logicWindow::cpuCycle));
             nextButton.set_border_width(10);
@@ -36,10 +45,36 @@ class logicWindow : public Gtk::Window{
             clockLabel.set_text("Clock (Hz): ");
             runClockLabel.set_text("Run Clock: ");
             signedOutLabel.set_text("Signed Out: ");
+            addressLabel.set_text("RAM Address: ");
+            valueLabel.set_text("Current Value (hex): ");
+            fileLabel.set_text("Load RAM File: ");
             
-            generalGrid.set_column_homogeneous(true);
-            generalGrid.attach(outFrame, 0, 0, 1, 1);
+            //generalGrid.set_column_homogeneous(true);
+            generalGrid.attach(outFrame , 0, 0, 1, 1);
+            generalGrid.attach(ramFrame , 1, 0, 1, 1);
             generalGrid.attach(timeFrame, 0, 1, 1, 1);
+            
+            ramLoad.set_border_width(5);
+            ramFileChooser.set_border_width(5);
+            ramFileChooser.signal_file_set().connect(sigc::mem_fun(*this, &logicWindow::open_ram_file));
+
+            ramAddress.set_margin_bottom(5);
+            ramValue.set_margin_bottom(5);
+            ramAddress.set_margin_left(5);
+            ramValue.set_margin_left(5);
+            ramValue.set_margin_right(5);
+            
+
+            ramFrame.add(ramGrid);
+            ramFrame.set_border_width(5);
+            ramGrid.set_column_homogeneous(true);
+            ramGrid.attach(fileLabel     , 0, 0, 1, 1);
+            ramGrid.attach(ramFileChooser, 1, 0, 2, 1);
+            ramGrid.attach(addressLabel  , 0, 1, 1, 1);
+            ramGrid.attach(ramAddress    , 1, 1, 1, 1);
+            ramGrid.attach(valueLabel    , 2, 1, 1, 1);
+            ramGrid.attach(ramValue      , 3, 1, 1, 1);
+            ramGrid.attach(ramLoad       , 3, 0, 1, 1);
             
             outFrame.add(outGrid);
             outFrame.set_border_width(5);
@@ -61,17 +96,87 @@ class logicWindow : public Gtk::Window{
             
             add(generalGrid);
             show_all_children();
-     }
+        }
+    protected:
+        logic<STEPS_PER_INSTRUCTION> CPU;
+
+        sigc::connection timeoutConn;
+        sigc::connection updateConn;
         
+        Gtk::Grid generalGrid;
+
+        Gtk::Frame ramFrame;
+        Gtk::Grid ramGrid;
+        Gtk::FileChooserButton ramFileChooser;
+        Gtk::SpinButton ramAddress;
+        Gtk::Entry ramValue;
+        Gtk::Label valueLabel;
+        Gtk::Label addressLabel;
+        Gtk::Label fileLabel;
+        Gtk::Button ramLoad;
+
+        Gtk::Frame outFrame;
+        Gtk::Grid outGrid;
+        Gtk::Label signedOutLabel;
+        Gtk::Label outputLabel;
+        Gtk::Label output;
+        Gtk::Switch signedOut;
+
+        Gtk::Frame timeFrame;
+        Gtk::Grid timeGrid;
+        Gtk::Button nextButton;
+        Gtk::Button nextInstructionButton;
+        Gtk::HScale clockSpeed;
+        Gtk::Label clockLabel;
+        Gtk::Label runClockLabel;
+        Gtk::Switch runFree;
+
+        void change_ram(){
+            try{
+                CPU.ram[ramAddress.get_value_as_int()] = static_cast<uint8_t>(std::stoi(ramValue.get_text(), 0, 16));
+            }catch(std::exception e){
+                std::cout << e.what() << std::endl;
+            }
+            ramAddress.grab_focus_without_selecting();
+            //return false;
+        }
+
+        void load_ram(){
+            if(ramFileChooser.get_file())
+                open_ram_file();
+            else
+                CPU.ram = RAM::loadNull();
+        }
+
+        void open_ram_file(){
+            uint8_t ram[16];
+            ramFileChooser.get_file()->read()->read(ram, 16);
+            int i = 0;
+            CPU = logic<STEPS_PER_INSTRUCTION>();
+            for(auto &value : CPU.ram)
+                value = ram[i++];
+        }
+
         void cpuCycle(){
             if(!CPU.halt){
                 CPU.cycle();
-                if(!signedOut.get_active())
-                    output.set_text(std::to_string(CPU.outReg));
-                else
-                    output.set_text(std::to_string(static_cast<int8_t>(CPU.outReg)));
+                out_reg_update();
             }
         }
+
+        void out_reg_update(){
+            if(!signedOut.get_active())
+                output.set_text(std::to_string(CPU.outReg));
+            else
+                output.set_text(std::to_string(static_cast<int8_t>(CPU.outReg)));
+        }
+
+        void update_ram_value(){
+            std::stringstream strm;
+            strm << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(CPU.ram[ramAddress.get_value()]);
+            ramValue.set_text(std::string(strm.str()));
+        }
+
         void cpu_inst_callback(){
             int i;
             for(int i = 0; i < 5; i++)
@@ -81,6 +186,14 @@ class logicWindow : public Gtk::Window{
             cpuCycle();
             return true;
         }
+
+        bool update_callback(int arg){
+            if(!ramValue.is_focus())
+                update_ram_value();
+            out_reg_update();
+            return true;
+        }
+
         void set_clock(){
             if(runFree.get_active()){
                 if(timeoutConn.connected()){
@@ -96,25 +209,6 @@ class logicWindow : public Gtk::Window{
         void set_runFree(Gtk::StateFlags flags){
             set_clock();
         }
-        
-        
-    protected:
-        sigc::connection timeoutConn;
-        Gtk::Switch runFree;
-        Gtk::Switch signedOut;
-        Gtk::HScale clockSpeed;
-        Gtk::Button nextButton;
-        Gtk::Button nextInstructionButton;
-        Gtk::Frame timeFrame;
-        Gtk::Frame outFrame;
-        Gtk::Grid generalGrid;
-        Gtk::Grid timeGrid;
-        Gtk::Grid outGrid;
-        Gtk::Label signedOutLabel;
-        Gtk::Label outputLabel;
-        Gtk::Label output;
-        Gtk::Label clockLabel;
-        Gtk::Label runClockLabel;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -9,14 +9,8 @@
 
 
 int main(int argc, char** argv){
-    std::array<std::uint8_t, RAM::RAM_SIZE> ram;
-    std::array<std::array<std::uint16_t, STEPS_PER_INSTRUCTION>, 16> instructions = logic<STEPS_PER_INSTRUCTION>::defaultInstructions;
-    //logic<STEPS_PER_INSTRUCTION> CPU(RAM::loadNull(), instructions, true);
     std::cout << "Ben Eater's 8-bit Computer Emulator\nMade by Rubem Nobre @ https://github.com/rubemnobre/eater-emulator" << std::endl;
-    //FILE* ramFile = fopen("example-programs/fibonacci.ram", "rb");
-    //ram = RAM::loadFromFile(ramFile);
     auto gui = Gtk::Application::create(argc, argv, "com.rubemjrx.eater.emulator");
     logicWindow guiWindow;
-    //guiWindow.CPU.ram = ram;
     return gui->run(guiWindow);
 }
